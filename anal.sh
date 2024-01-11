ab58bcd3ccbaf39baaae54f23331c3e28b84f999
mpoghos <martin.poghosyan@cern.ch>
2024-01-11 15:12:52 +0100

    Update anal.sh
    minor fixes

--- anal.sh
+++ anal.sh
@@ -15,9 +15,13 @@ root -b -q AnalData2textFile.C\($runNumber,1\)
 ./a.out $runNumber 0
 ./a.out $runNumber 1
 
+sleep 5
+
 cd pedestalsRun3
 tar -cvf ${runNumber}.tar $runNumber
 gzip ${runNumber}.tar
 
 cd ..
 echo "pedestalsRun3/${runNumber}.tar.gz produced"
+
+#scp pedestalsRun3/${runNumber}.tar.gz poghos@lxplus.cern.ch:/eos/home-p/poghos/P2_data/PedRun/.
